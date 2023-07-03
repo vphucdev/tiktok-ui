9fd4c130b2dc55b67e9da0e4c1d6728748cede3b
“phucdev” <vanphuc0295@gmail.com>
2023-07-03 21:00:57 +0700

    export default

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import { Fragment } from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import { publicRoutes } from './routes';
-import { DefaultLayout } from '~/layouts';
+import DefaultLayout  from '~/layouts';
 function App() {
     return (
         <Router>
