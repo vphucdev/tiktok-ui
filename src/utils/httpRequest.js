4f6cde549d08c8c52d0694b61627d495093e970e
“phucdev” <vanphuc0295@gmail.com>
2023-07-03 21:28:44 +0700

    create .env

--- src/utils/httpRequest.js
+++ src/utils/httpRequest.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const httpRequest = axios.create({
-    baseURL: 'https://tiktok.fullstack.edu.vn/api/',
+    baseURL: process.env.REACT_APP_BASE_URL,
 });
 
 httpRequest.interceptors.response.use((response) => {
