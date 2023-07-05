c45f7a1f73307f1d4f06bf2789797af754387454
“phucdev” <vanphuc0295@gmail.com>
2023-07-05 22:21:12 +0700

    update

--- src/layouts/components/Sidebar/index.js
+++ src/layouts/components/Sidebar/index.js
@@ -1,13 +1,1 @@
-import classNames from "classnames/bind";
-import styles from './Sidebar.module.scss'
-
-const cx = classNames.bind(styles)
-function Sidebar() {
-    return (
-        <aside className={cx('wrapper')}>
-            <div className={cx('content')}></div>
-        </aside>
-    );
-}
-
-export default Sidebar;
+export {default} from './Sidebar';
