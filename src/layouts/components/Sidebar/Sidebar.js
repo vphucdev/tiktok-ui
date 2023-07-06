b458de1fac9f566df957fc1e611b95331fe58640
“phucdev” <vanphuc0295@gmail.com>
2023-07-06 15:59:57 +0700

    add iconActive

--- src/layouts/components/Sidebar/Sidebar.js
+++ src/layouts/components/Sidebar/Sidebar.js
@@ -3,17 +3,17 @@ import classNames from 'classnames/bind';
 import styles from './Sidebar.module.scss';
 import Menu, { MenuItem } from './Menu/';
 import config from '~/config';
-import { ExploreIcon, HomeIcon, LiveIcon, UserGroupIcon } from '~/components/Icons';
+import { ExploreIcon, ExploreIconActive, HomeIcon, HomeIconActive, LiveIcon, LiveIconActive, UserGroupIcon, UserGroupIconActive } from '~/components/Icons';
 
 const cx = classNames.bind(styles);
 function Sidebar() {
     return (
         <aside className={cx('wrapper')}>
             <Menu>
-                <MenuItem to={config.routes.home} title='Dành cho bạn' icon={<HomeIcon />} />
-                <MenuItem to={config.routes.following} title='Đang Follow' icon={<UserGroupIcon />} />
-                <MenuItem to={config.routes.explore} title='Khám Phá' icon={<ExploreIcon />} />
-                <MenuItem to={config.routes.live} title='LIVE' icon={<LiveIcon />} />
+                <MenuItem to={config.routes.home} title='Dành cho bạn' icon={<HomeIcon />} iconActive={<HomeIconActive />} />
+                <MenuItem to={config.routes.following} title='Đang Follow' icon={<UserGroupIcon />}iconActive={<UserGroupIconActive />} />
+                <MenuItem to={config.routes.explore} title='Khám Phá' icon={<ExploreIcon />} iconActive={<ExploreIconActive />}/>
+                <MenuItem to={config.routes.live} title='LIVE' icon={<LiveIcon />} iconActive={<LiveIconActive />}/>
             </Menu>
         </aside>
     );
