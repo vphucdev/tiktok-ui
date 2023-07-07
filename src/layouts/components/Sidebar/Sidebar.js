e12c7302fa5dac25a0b1bb97a962747e42ad91b8
“phucdev” <vanphuc0295@gmail.com>
2023-07-07 20:19:01 +0700

    add  SuggestedAccountItems in Sdibar

--- src/layouts/components/Sidebar/Sidebar.js
+++ src/layouts/components/Sidebar/Sidebar.js
@@ -3,18 +3,43 @@ import classNames from 'classnames/bind';
 import styles from './Sidebar.module.scss';
 import Menu, { MenuItem } from './Menu/';
 import config from '~/config';
-import { ExploreIcon, ExploreIconActive, HomeIcon, HomeIconActive, LiveIcon, LiveIconActive, UserGroupIcon, UserGroupIconActive } from '~/components/Icons';
-
+import {
+    ExploreIcon,
+    ExploreIconActive,
+    HomeIcon,
+    HomeIconActive,
+    LiveIcon,
+    LiveIconActive,
+    UserGroupIcon,
+    UserGroupIconActive,
+} from '~/components/Icons';
+import SuggestedAccountItems from '~/components/SuggestedAccountItems/SuggestedAccountItems';
 const cx = classNames.bind(styles);
 function Sidebar() {
     return (
         <aside className={cx('wrapper')}>
             <Menu>
-                <MenuItem to={config.routes.home} title='Dành cho bạn' icon={<HomeIcon />} iconActive={<HomeIconActive />} />
-                <MenuItem to={config.routes.following} title='Đang Follow' icon={<UserGroupIcon />}iconActive={<UserGroupIconActive />} />
-                <MenuItem to={config.routes.explore} title='Khám Phá' icon={<ExploreIcon />} iconActive={<ExploreIconActive />}/>
-                <MenuItem to={config.routes.live} title='LIVE' icon={<LiveIcon />} iconActive={<LiveIconActive />}/>
+                <MenuItem
+                    to={config.routes.home}
+                    title="Dành cho bạn"
+                    icon={<HomeIcon />}
+                    iconActive={<HomeIconActive />}
+                />
+                <MenuItem
+                    to={config.routes.following}
+                    title="Đang Follow"
+                    icon={<UserGroupIcon />}
+                    iconActive={<UserGroupIconActive />}
+                />
+                <MenuItem
+                    to={config.routes.explore}
+                    title="Khám Phá"
+                    icon={<ExploreIcon />}
+                    iconActive={<ExploreIconActive />}
+                />
+                <MenuItem to={config.routes.live} title="LIVE" icon={<LiveIcon />} iconActive={<LiveIconActive />} />
             </Menu>
+            <SuggestedAccountItems label="Các tài khoản đang follow" more="Xem thêm" />
         </aside>
     );
 }
