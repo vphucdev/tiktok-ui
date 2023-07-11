e7644e40867cc0f1a95456c1c0ba7e1177b0cc07
“phucdev” <vanphuc0295@gmail.com>
2023-07-11 15:20:18 +0700

    export home page

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,5 @@
 export { default as Following } from './Following'
-export { default as Home } from './Home'
+export { default as Home } from './Home/Home'
 export { default as Profile } from './Profile'
 export { default as Upload } from './Upload'
 export { default as Search } from './Search'
