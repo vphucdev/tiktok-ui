45f53bac5339be1004165187ec2603e9ab0f615a
“phucdev” <vanphuc0295@gmail.com>
2023-07-18 09:32:02 +0700

    down to version react-router-dom

--- src/config/routes.js
+++ src/config/routes.js
@@ -1,7 +1,7 @@
 const routes = {
     home: '/',
     following: '/following',
-    profile: '/:nickname',
+    profile: '/@:nickname',
     upload: '/upload',
     search: '/search',
     live: '/live',
